4c6e7661c634c40f45743647bfa9ec345a7199a0
whyseman <mercy719@gmail.com>
2020-09-21 19:35:29 -0600

    modded

--- module2-sql-for-analysis/titanic_practice.py
+++ module2-sql-for-analysis/titanic_practice.py
@@ -1,13 +1,12 @@
 import psycopg2
 
-conn = psycopg2.connect(database="nxbwnxng",
+conn = psycopg2.connect(dbname="nxbwnxng",
                         user="nxbwnxng",
                         password="33E4H6D_wF7NYnSjhknkOunR4FK7dCLi",
                         host="rogue.db.elephantsql.com")
 
 cur = conn.cursor()
 
-cur.execute("SELECT *"
-            "FROM test_table;")
+cur.execute("SELECT * FROM test_table;")
 
-cur.fetchall()
+cur.fetchone()
